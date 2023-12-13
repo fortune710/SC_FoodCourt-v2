b314a606b723d245cf063d113eb4d6b4d168671f
fortune710 <fortune0208@yahoo.com>
2023-12-13 20:46:01 +0100

    testing jsx

---
+++ app/hello.jsx
@@ -0,0 +1,14 @@
+import { Page, Text } from "../components/Themed";
+//You can only use Relative Imports in JSX and not import aliases
+
+const HelloPage = () => {
+    return (
+        <Page>
+
+        </Page>
+    )
+}
+
+//This Page does not exist in the design
+//I was just checking if you can use jsx
+export default HelloPage;
