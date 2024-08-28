bf8241a2d3077d0a50b02c25bb40f243e34ca08a
fortune710 <fortune0208@yahoo.com>
2024-08-27 23:45:20 -0400

    changes

--- components/CategoriesList.tsx
+++ components/CategoriesList.tsx
@@ -2,25 +2,58 @@ import React, { useState } from "react"
 import { View, FlatList, Pressable, Text, StyleSheet } from "react-native";
 import { Image } from "expo-image";
 import Styles from "../constants/Styles";
+import { X } from "lucide-react-native";
 
-const Categories = [
-    'Rice',
-    'Sharwama',
-    'Pasta',
-    'Grills',
-    'Wraps'
+export const CATEGORIES = [
+    {
+        name: "Rice",
+        image: require("../assets/images/rice-bowl.svg")
+    },
+    {
+        name: "Pasta",
+        image: require("../assets/images/pasta.svg")
+    },
+    {
+        name: "Sharwama",
+        image: require("../assets/images/sharwama.svg")
+    },
+    {
+        name: "Drinks",
+        image: require("../assets/images/rice-bowl.svg")
+    },
+    {
+        name: "Desserts",
+        image: require("../assets/images/rice-bowl.svg")
+    },
+    {
+        name: "Grills",
+        image: require("../assets/images/grills.svg")
+    },
+    {
+        name: "Sandwiches",
+        image: require("../assets/images/rice-bowl.svg")
+    },
+    {
+        name: "Burgers",
+        image: require("../assets/images/burger.svg")
+    }
 ] as const
 
-type Category = typeof Categories[number];
+type Category = typeof CATEGORIES[number]['name'];
 
 interface CategoriesListProps {
-    onChange?: (category: Category) => void
+    onChange?: (category: Category|"all") => void
 }
 
 const CategoriesList: React.FC<CategoriesListProps> = ({ onChange }) => {
     const [_, setCategory] = useState<Category>();
 
     const handleChange = (category: Category) => {
+        if (category === _) {
+            setCategory(undefined);
+            onChange && onChange("all");
+            return
+        }
         setCategory(category);
         onChange && onChange(category)
     }
@@ -32,15 +65,17 @@ const CategoriesList: React.FC<CategoriesListProps> = ({ onChange }) => {
             </Text>
             <FlatList
                 horizontal
-                data={Categories}
-                style={{}}                
+                data={CATEGORIES}
                 showsHorizontalScrollIndicator={false}
                 renderItem={({ item: category }) => (
-                    <Pressable onPress={() => handleChange(category)} style={styles.horizontalListItem}>
+                    <Pressable onPress={() => handleChange(category.name)} style={styles.horizontalListItem}>
                         <View style={styles.itemContainer}>
-                            <Image style={{ width: 50, height: 50 }} source={require("../assets/images/rice-bowl.svg")} />
+                            {
+                                category.name === _ ? <X /> :
+                                <Image style={{ width: 50, height: 50 }} source={category.image} />
+                            }
                         </View>
-                        <Text>{category}</Text>
+                        <Text>{category.name}</Text>
                     </Pressable>
                 )}
             />
