f05aa8eba07732ccecdca6dfb73366d9b9ff24e1
fortune710 <fortune0208@yahoo.com>
2024-10-05 22:50:49 -0400

    extracted logic into component

--- app/item/[id].tsx
+++ app/item/[id].tsx
@@ -1,125 +1,112 @@
-import { Pressable, ScrollView, View } from "react-native";
+import { Pressable, ScrollView, View, Text as RnText, TouchableOpacity } from "react-native";
 import { Page, Text } from "../../components/Themed";
 import useThemeColor from "../../hooks/useThemeColor";
 import { useLocalSearchParams, useRouter } from "expo-router";
 import { ImageBackground } from "expo-image";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
 import useSingleRestaurant from "@/hooks/useSingleResturant";
-import { Button, ListItem } from "@rneui/themed";
 import useCart from "@/hooks/useCart";
 import useCurrentUser from "@/hooks/useCurrentUser";
-import { Clock } from "lucide-react-native";
+import { Clock, Minus, Plus } from "lucide-react-native";
 import { useState } from "react";
-
-import products from "@/mock/products.json";
+import { SafeAreaView } from "react-native-safe-area-context";
+import MenuItemAddons from "@/components/MenuItemAddons";
 
 export default function MenuItemDetail() {
     const router = useRouter();
     const { id, resturantId } = useLocalSearchParams();
-    const [expanded, setExpanded] = useState(false);
 
 
     const { getSingleMenuItem } = useSingleRestaurant(Number(resturantId));
     const menuItem = getSingleMenuItem(Number(id));
     const [quantity, setQuantity] = useState(1);
     const { currentUser } = useCurrentUser();
+
     const { addItem } = useCart(currentUser?.id!);
+
+    const primary = useThemeColor({}, "primary");
+
+    const reduceQuantity = () => {
+        if (quantity <= 1) return
+        return setQuantity(quantity - 1)
+    }
+
+    const increaseQuantity = () => setQuantity(quantity + 1);
+
+    const addItemToCart = () => {
+        return addItem({
+            user_id: currentUser?.id!,
+            menu_item_id: menuItem?.id!,
+            quantity: quantity,
+        })
+    }
+
+    const totalPrice = (menuItem?.price || 0) * quantity
 
 
     return (
-        <Page className="flex-1 bg-white">
-            <View className="relative h-48">
-                <ImageBackground
-                    source={require("../../assets/images/food.png")}
-                    className="w-full h-full"
-                    resizeMode="cover"
-                >
-                    <View className="absolute top-0 left-0 p-4 flex-row justify-between items-center w-full">
-                        <Pressable onPress={() => router.back()}>
-                            <FontAwesome name="bars" size={24} color="black" />
-                        </Pressable>
-                        <Text className="text-black text-lg font-semibold">Mexican Shawarma</Text>
-                        <FontAwesome name="shopping-cart" size={24} color="red" />
-                    </View>
-                </ImageBackground>
-            </View>
-
-            <ScrollView className="p-4 space-y-4">
-                <Text className="text-xl font-semibold">{menuItem?.name}</Text>
-                <Text className="text-gray-500">{menuItem?.description || "Lorem ipsum dolor sit amet, consectetur adipiscing elit."}</Text>
-                
-                <View className="flex-row items-center space-x-4">
-                    <Pressable onPress={() => setQuantity(quantity > 1 ? quantity - 1 : 1)} className="p-2 bg-gray-200 rounded-full">
-                        <FontAwesome name="minus" size={16} color="black" />
-                    </Pressable>
-                    <Text className="text-lg">{quantity}</Text>
-                    <Pressable onPress={() => setQuantity(quantity + 1)} className="p-2 bg-gray-200 rounded-full">
-                        <FontAwesome name="plus" size={16} color="black" />
-                    </Pressable>
+        <SafeAreaView>
+            <Page className="flex-1 bg-white">
+                <View className="relative h-48">
+                    <ImageBackground
+                        source={{ uri: require("@/assets/images/food.png")}}
+                        className="w-full h-full"
+                        resizeMode="cover"
+                    >
+                        <View className="absolute top-0 left-0 p-4 flex-row justify-between items-center w-full">
+                            <Pressable onPress={() => router.back()}>
+                                <FontAwesome name="bars" size={24} color="black" />
+                            </Pressable>
+                            <Text className="text-black text-2xl font-semibold">{menuItem?.name}</Text>
+                            <FontAwesome name="shopping-cart" size={24} color="red" />
+                        </View>
+                    </ImageBackground>
                 </View>
 
-                <View className="flex flex-row justify-between w-full">
-                    <View>
-                        <Text>{menuItem?.name}</Text>
+                <ScrollView className="p-4 space-y-4">
+
+                    <View className="flex flex-row items-center">
+                        <View className="w-2/3">
+                            <Text className="text-xl font-semibold">{menuItem?.name}</Text>
+                            <Text className="text-gray-500">{menuItem?.description || "Lorem ipsum dolor sit amet, consectetur adipiscing elit."}</Text>
+                        </View>
+                        
+                        <View className="flex flex-row items-center gap-4">
+                            <TouchableOpacity onPress={reduceQuantity} className="p-1 border border-primary rounded-full">
+                                <Minus stroke={primary} className="text-primary"/>
+                            </TouchableOpacity>
+
+                            <Text className="text-xl">{quantity}</Text>
+
+                            <TouchableOpacity onPress={increaseQuantity} className="p-1 border border-primary rounded-full">
+                                <Plus stroke={primary} className="text-primary"/>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+
+                    <View className="my-3">
                         <Text>Category: {menuItem?.category}</Text>
                     </View>
 
-                    <View style={{ display: "flex", flexDirection: "row", width: 300, justifyContent: "space-around" }}>
-                        <Pressable onPress={() => setQuantity(quantity + 1)}>
-                            <FontAwesome name="plus"/>
-                        </Pressable>
+                    <View className="flex flex-row items-center gap-2">
+                        <Clock stroke={primary}/>
+                        <Text>{menuItem?.preparation_time!}</Text>
                     </View>
-                </View>
 
-                <View>
-                    <Clock/>
-                    <Text>{menuItem?.preparation_time!}</Text>
-                </View>
-
-                
-                <ListItem.Accordion
-                    content={
-                        <ListItem.Content>
-                            <ListItem.Title>Add Ons</ListItem.Title>
-                        </ListItem.Content>
-                    }
-                    isExpanded={expanded}
-                    onPress={() => {
-                        setExpanded(!expanded);
-                    }}
+                    <MenuItemAddons addons={menuItem?.add_ons!}/>
+                    
+                    <TouchableOpacity 
+                        onPress={addItemToCart} 
+                        className="bg-primary rounded-3xl py-4 flex flex-row items-center justify-center mt-32"
                     >
-                        {
-                            menuItem?.add_ons?.map((item) => (
-                                <ListItem key={item.foodName} bottomDivider>
-                                    <ListItem.Content>
-                                        <ListItem.Title>{item.foodName}</ListItem.Title>
-                                        <ListItem.Subtitle>{item.price}</ListItem.Subtitle>
-                                    </ListItem.Content>
-                                    <ListItem.Chevron />
-                                </ListItem>
-
-                            ))
-                        }
-                </ListItem.Accordion>
-
-
-                <Button 
-                    onPress={() => addItem({
-                        user_id: currentUser?.id!,
-                        menu_item_id: menuItem?.id!,
-                        quantity: quantity,
-                    })} 
-                    color="#F72F2F" 
-                    style={{ alignSelf: "center", width: "100%" }} 
-                    titleStyle={{ textAlign: "center", padding: 32 }}
-                >
-                    <Text>Add to Cart (N {(menuItem?.price || 0) * quantity})</Text>    
-                </Button>
+                        <RnText className="text-white font-medium">Add to Cart (N {totalPrice})</RnText>    
+                    </TouchableOpacity>
 
 
 
 
-            </ScrollView>
-        </Page>
+                </ScrollView>
+            </Page>
+        </SafeAreaView>
     );
 }
