478a6c3149770dbdaddadee0717c67f18c45b1a4
fortune710 <fortune0208@yahoo.com>
2024-10-16 13:26:53 -0400

    feat: setup cart items

--- components/CartItem.tsx
+++ components/CartItem.tsx
@@ -1,3 +1,4 @@
+import useRestaurant from '@/hooks/useRestaurant';
 import { Text } from '../components/Themed';
 import React, { useState } from 'react';
 import { View, StyleSheet, Pressable } from 'react-native';
@@ -6,112 +7,115 @@ import Feather from 'react-native-vector-icons/Feather';
 
 
 interface CartItemProps {
-    name: string;
-    description: string;
-    price: number;
-    quantity: number;
-  }
+  name: string;
+  description: string;
+  price: number;
+  quantity: number;
+  restaurantId: number;
+}
   
-  const CartItem: React.FC<CartItemProps> = ({ name, description, price, quantity }) => {
-    const [qty, setQty] = useState(quantity);
+const CartItem: React.FC<CartItemProps> = ({ name, description, price, quantity, restaurantId }) => {
+  const [qty, setQty] = useState(quantity);
+  const { data: restaurants } = useRestaurant();
 
-    const handleIncrement = () => qty === 20 ? null : setQty(qty + 1);
-  
-    const handleDecrement = () => qty === 1 ? null : setQty(qty - 1);
-  
-    const store = 'African pot'
+  const restaurant = restaurants?.find(({ id }) => id === restaurantId);
 
-    return (
-      <View style={styles.container}>
-        <View style = {{flexDirection: 'row', justifyContent:'space-between'}}>
-          <View style = {{flexDirection: 'row'}}>
-            <Text style={styles.title}>{name} - </Text>
-            <Text style={{fontWeight: 'bold',fontSize: 15, color:'#f72f2f'}}>{store}</Text>
-          </View>
-          <View style={styles.priceContainer}>
-            <Text style={styles.priceLabel}># {price}</Text>
-          </View>
+
+  const handleIncrement = () => qty === 20 ? null : setQty(qty + 1);
+
+  const handleDecrement = () => qty === 1 ? null : setQty(qty - 1);
+
+  return (
+    <View style={styles.container}>
+      <View style = {{flexDirection: 'row', justifyContent:'space-between'}}>
+        <View style = {{flexDirection: 'row'}}>
+          <Text style={styles.title}>{name} - </Text>
+          <Text style={{fontWeight: 'bold',fontSize: 15, color:'#f72f2f'}}>{restaurant?.name}</Text>
         </View>
-        <View style={styles.descriptionContainer}>
-          <Text style={styles.description}>{description}</Text>
-        </View>
-        <View style={styles.actionsContainer}>
-          <Pressable onPress={handleIncrement} style={styles.actionButton}>
-            <View style={styles.iconContainer}>
-              <Feather name="plus-circle" size={18} color="white" />
-            </View>
-          </Pressable>
-          <Text style={styles.amountText}>{qty}</Text>
-          <Pressable onPress={handleDecrement} style={styles.actionButton}>
-            <View style={styles.iconContainer}>
-              <Feather name="minus-circle" size={18} color="white" />
-            </View>
-          </Pressable>
+        <View style={styles.priceContainer}>
+          <Text style={styles.priceLabel}>N {price * qty}</Text>
         </View>
       </View>
-    );
-  };
+      <View style={styles.descriptionContainer}>
+        <Text style={styles.description}>{description}</Text>
+      </View>
+      <View style={styles.actionsContainer}>
+        <Pressable onPress={handleIncrement} style={styles.actionButton}>
+          <View style={styles.iconContainer}>
+            <Feather name="plus-circle" size={18} color="white" />
+          </View>
+        </Pressable>
+        <Text style={styles.amountText}>{qty}</Text>
+        <Pressable onPress={handleDecrement} style={styles.actionButton}>
+          <View style={styles.iconContainer}>
+            <Feather name="minus-circle" size={18} color="white" />
+          </View>
+        </Pressable>
+      </View>
+    </View>
+  );
+};
   
-  const styles = StyleSheet.create({
-    container: {
-      width: '90%',
-      height: 100,
-      alignSelf: 'center',
-      marginBottom: 20,
-    },
-    title: {
-      fontWeight: 'bold',
-      fontSize: 15,
-    },
-    descriptionContainer: {
-      flexDirection: 'row',
-      justifyContent: 'space-between',
-      marginTop: 2,
-    },
-    description: {
-      fontSize: 13,
-      width: '70%',
+const styles = StyleSheet.create({
+  container: {
+    width: '90%',
+    height: 100,
+    alignSelf: 'center',
+    marginBottom: 20,
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 15,
+  },
+  descriptionContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: 2,
+  },
+  description: {
+    fontSize: 13,
+    width: '70%',
 
-    },
-    priceContainer: {
-      width: '23%',
-      height: 30,
-      borderWidth: 1.5,
-      borderColor: '#fe0000',
-      backgroundColor: '#fe00002e',
-      borderRadius: 50,
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    priceLabel: {
-      fontSize: 14,
-      fontWeight: 'bold',
-      color: '#fe0000',
-      marginHorizontal: 2,
-    },
-    actionsContainer: {
-      flexDirection: 'row',
-      justifyContent: 'space-between',
-      width: '27%',
-      marginTop:10,
-    },
-    actionButton: {
-      backgroundColor: '#f7941e',
-      width: 30,
-      height: 25,
-      borderRadius: 7,
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    iconContainer: {
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    amountText: {
-      textAlign: 'center',
-      fontSize: 18,
-      fontWeight: '500',
-    },
-  });
-  
-  export default CartItem;
+  },
+  priceContainer: {
+    width: '23%',
+    height: 30,
+    borderWidth: 1.5,
+    borderColor: '#fe0000',
+    backgroundColor: '#fe00002e',
+    borderRadius: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  priceLabel: {
+    fontSize: 14,
+    fontWeight: 'bold',
+    color: '#fe0000',
+    marginHorizontal: 2,
+  },
+  actionsContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '27%',
+    marginTop:10,
+  },
+  actionButton: {
+    backgroundColor: '#f7941e',
+    width: 30,
+    height: 25,
+    borderRadius: 7,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  iconContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  amountText: {
+    textAlign: 'center',
+    fontSize: 18,
+    fontWeight: '500',
+  },
+});
+
+export default CartItem;
