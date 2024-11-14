5c188c9ee7c6aaa806c8f0cd712b1936002589c0
Fortune Oluwasemilore Alebiosu <fortune0208@yahoo.com>
2024-11-14 01:00:54 -0500

    fixed web build

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -1,13 +1,39 @@
 import 'react-native-url-polyfill/auto';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
+import { Platform } from 'react-native';
 
 const supabaseUrl = "https://xfcyfgduhwcuftrcijta.supabase.co";
 const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhmY3lmZ2R1aHdjdWZ0cmNpanRhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTMxMDE4MTcsImV4cCI6MjAyODY3NzgxN30.sTOrXGoSclTtSVIMOSbPWTgHUpwh0wryqhH7vxRmvf0";
 
+
+class SupabaseStorage {
+  async getItem(key: string) {
+    if (Platform.OS === "web") {
+      if (typeof localStorage === "undefined") {
+        return null;
+      }
+      return localStorage.getItem(key);
+    }
+    return AsyncStorage.getItem(key);
+  }
+  async removeItem(key: string) {
+    if (Platform.OS === "web") {
+      return localStorage.removeItem(key);
+    }
+    return AsyncStorage.removeItem(key);
+  }
+  async setItem(key: string, value: string) {
+    if (Platform.OS === "web") {
+      return localStorage.setItem(key, value);
+    }
+    return AsyncStorage.setItem(key, value);
+  }
+}
+
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
-    storage: AsyncStorage,
+    storage: new SupabaseStorage(),
     autoRefreshToken: true,
     persistSession: true,
     detectSessionInUrl: false,
